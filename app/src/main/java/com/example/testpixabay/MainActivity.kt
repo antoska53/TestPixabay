7641dde2552e8e7da9d6933c971414cb1d3c0f4b
Antoska <tosha170@outlook.com>
2023-02-10 17:25:20 +0300

    fix icon
    поправил отображение иконки на splashscreen

--- app/src/main/java/com/example/testpixabay/MainActivity.kt
+++ app/src/main/java/com/example/testpixabay/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.testpixabay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
+        var keepSplashOnScreen = true
+        val delay = 1000L
+        installSplashScreen().setKeepOnScreenCondition { keepSplashOnScreen }
+        Handler(Looper.getMainLooper()).postDelayed({ keepSplashOnScreen = false }, delay)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
